c7febdbf1aa947e25e71b31081ebb8e241c524c2
argilyazov <arsel,giliazov@urfu.me>
2023-06-20 13:49:53 +0500

    i18n

---
+++ src/main/java/com/example/demo/config/ExampleConfig.java
@@ -0,0 +1,36 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.thymeleaf.templatemode.TemplateMode;
+
+import java.util.Locale;
+import java.util.TimeZone;
+
+@Configuration
+public class ExampleConfig implements WebMvcConfigurer {
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("ru"));
+        return localeResolver;
+    }
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("localeData");
+        return localeChangeInterceptor;
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry interceptorRegistry) {
+        var interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        interceptorRegistry.addInterceptor(interceptor);
+    }
+
+}
